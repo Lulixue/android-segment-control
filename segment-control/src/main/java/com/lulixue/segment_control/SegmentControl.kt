38eae64ef5b7155cc1198c6fa086c2e6a1211495
xiaoligen <jackreagan@163.com>
2021-10-08 16:19:39 +0800

    add selected item type

--- segment-control/src/main/java/com/lulixue/segment_control/SegmentControl.kt
+++ segment-control/src/main/java/com/lulixue/segment_control/SegmentControl.kt
@@ -4,56 +4,74 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
-import android.view.ViewGroup
+import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
 class SegmentControl(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+    companion object {
+        private const val DEFAULT_SELECTED_BG_COLOR = Color.WHITE
+        private val DEFAULT_SEPARATOR_COLOR = Color.parseColor("#CBCBCF")
+        private val DEFAULT_BACKGROUND_COLOR = Color.parseColor("#EEEEEF")
+    }
     private var roundRadius: Float = 5.dp
-    private var roundStrokeWidth: Float = 0f
-    private var roundStrokeColor = Color.GRAY
-    private var itemPaddingStart = 5.dp
-    private var itemPaddingRight = 5.dp
-    private var itemPaddingTop = 3.dp
-    private var itemPaddingBottom = 3.dp
+    private var itemPaddingStart = 10.dp
+    private var itemPaddingEnd = 10.dp
+    private var itemPaddingTop = 5.dp
+    private var itemPaddingBottom = 5.dp
     private var itemTextSize = 13.sp
-    private var fixedWidth = true
-    private var items = ArrayList<String>().apply {
+    private var fixedWidth = false
+    private var separatorColor = DEFAULT_SEPARATOR_COLOR
+    private var itemBackgroundColor = DEFAULT_BACKGROUND_COLOR
+    private var selectedBackgroundColor = DEFAULT_SELECTED_BG_COLOR
+    private var selectedRoundPadding: Float = 2.dp
+    private val separatorWidth = 1.dp
+    private var separatorHeight = 0f
+    private var selectedPosition = 0
+
+    private val items = ArrayList<String>().apply {
         add("First")
         add("Second")
         add("Third")
     }
+
     private var itemWidths = ArrayList<Float>()
     private var bound = Rect()
 
-    private var separatorPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+    private var fillPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        strokeWidth = 1.dp
+    }
+
+    private var linePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         strokeWidth = 1.dp
         strokeCap = Paint.Cap.ROUND
-        color = Color.GRAY
     }
 
     private var textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         textSize = itemTextSize
+        textAlign = Paint.Align.CENTER
     }
-
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
         val widthMode = MeasureSpec.getMode(widthMeasureSpec)
-        val widthSpec = MeasureSpec.getSize(widthMeasureSpec)
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
         val heightMode = MeasureSpec.getMode(heightMeasureSpec)
-        val heightSpec = MeasureSpec.getSize(heightMeasureSpec)
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
 
         var maxHeight = 0
         var maxWidth = 0
         itemWidths.clear()
+        separatorHeight = 0f
         items.forEach {
             textPaint.getTextBounds(it, 0, it.length, bound)
+            separatorHeight = max(separatorHeight, bound.height().toFloat())
             itemWidths.add(bound.width().toFloat())
             maxWidth = max(maxWidth, bound.width())
             maxHeight = max(maxHeight, bound.height())
         }
+        separatorHeight += 2.dp
         var totalWidth = 0
         if (fixedWidth) {
             totalWidth = items.size * maxWidth
@@ -63,15 +81,15 @@ class SegmentControl(context: Context, attrs: AttributeSet?) : View(context, att
             }
         }
 
-        totalWidth += (3 * (itemPaddingStart + itemPaddingRight)).toInt()
+        totalWidth += (3 * (itemPaddingStart + itemPaddingEnd + 2 * selectedRoundPadding)).toInt()
 
-        val wrapContentHeight =  (maxHeight + itemPaddingTop + itemPaddingBottom).toInt()
+        val wrapContentHeight = (maxHeight + itemPaddingTop + itemPaddingBottom + 2 * selectedRoundPadding).toInt()
         val destHeight = when(heightMode) {
             MeasureSpec.AT_MOST -> {
-                min(wrapContentHeight, heightSpec)
+                min(wrapContentHeight, heightSize)
             }
             MeasureSpec.EXACTLY -> {
-                heightSpec
+                heightSize
             }
             else -> {
                 wrapContentHeight
@@ -79,20 +97,20 @@ class SegmentControl(context: Context, attrs: AttributeSet?) : View(context, att
         }
         val destWidth = when (widthMode) {
             MeasureSpec.AT_MOST -> {
-                min(totalWidth, widthSpec)
+                min(totalWidth, widthSize)
             }
             MeasureSpec.EXACTLY -> {
-                widthSpec
+                widthSize
             }
             else -> {
                 totalWidth
             }
         }
-        val itemWidth = (destWidth - 3 * (itemPaddingStart + itemPaddingRight)) / items.size
+        val itemWidth = destWidth - 6 * selectedRoundPadding
         if (fixedWidth) {
             itemWidths.clear()
             repeat(items.size) {
-                itemWidths.add(itemWidth)
+                itemWidths.add(itemWidth / items.size)
             }
         } else {
             var totalItemWidth = 0f
@@ -112,17 +130,43 @@ class SegmentControl(context: Context, attrs: AttributeSet?) : View(context, att
         super.draw(canvas)
 
         canvas.save()
-        separatorPaint.color = Color.GRAY
-        canvas.drawRoundRect(0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat(), roundRadius,  roundRadius, separatorPaint)
+        fillPaint.color = itemBackgroundColor
+        canvas.drawRoundRect(0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat(),
+                            roundRadius,  roundRadius, fillPaint)
 
-        var textX = itemPaddingStart
+        var startX = 0f
         for ((i, text) in items.withIndex()) {
-            canvas.drawText(text, textX, measuredHeight/2f, textPaint)
-            textX += itemWidths[i]
-            textX += itemPaddingRight
-            separatorPaint.color = Color.DKGRAY
-            canvas.drawRect(textX, itemPaddingTop,textX+1.5f, measuredHeight-itemPaddingBottom,  separatorPaint)
-            textX += itemPaddingStart
+
+            startX += selectedRoundPadding
+            if (selectedPosition == i) {
+                fillPaint.color = selectedBackgroundColor
+                canvas.drawRoundRect(startX, selectedRoundPadding,
+                            startX + itemWidths[i], measuredHeight - selectedRoundPadding,
+                                roundRadius,  roundRadius, fillPaint)
+                textPaint.typeface = Typeface.DEFAULT_BOLD
+            } else {
+                textPaint.typeface = Typeface.DEFAULT
+            }
+
+            val x = startX + itemWidths[i] / 2
+            val y = measuredHeight / 2f - (textPaint.descent() + textPaint.ascent()) / 2
+            canvas.drawText(text, x, y, textPaint)
+
+            startX += itemWidths[i]
+            startX += selectedRoundPadding
+            linePaint.color = separatorColor
+            if (i != items.lastIndex) {
+                if (abs(i - selectedPosition) > 0) {
+                    val startY = (measuredHeight - separatorHeight) / 2
+                    canvas.drawRect(
+                        startX,
+                        startY,
+                        startX + separatorWidth,
+                        startY + separatorHeight,
+                        linePaint
+                    )
+                }
+            }
         }
 
         canvas.restore()
